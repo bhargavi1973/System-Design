9148d4a34d1e70e13277dcd11c186967f5ea7462
Bhargavi Kanojia <165196422+bhargavi1973@users.noreply.github.com>
2026-07-09 19:25:38 +0530

    Implement Interface Segregation Principle for Shapes
    Refactor Shapes interface to adhere to Interface Segregation Principle by separating area and volume methods.

---
+++ 04-ISP/before.java
@@ -0,0 +1,46 @@
+interface Shapes {
+    void area();
+    void volume();
+}
+// Square, Rectangle are 2D shapes , so they throw an exception for volume method
+// and we have to write extra code for that, which is not a good design
+// This violates Interface Seggregation Principle (ISP)
+class Square implements Shapes {
+    @Override
+    public void area(){}
+
+    @Override
+    public void volume(){
+        throw new UnsupportedOperationException();
+    }
+}
+
+class Rectangle implements Shapes {
+    @Override
+    public void area(){}
+
+    @Override
+    public void volume(){
+        throw new UnsupportedOperationException();
+    }
+}
+// Cube is 3D shape, so it defines both area and volume methods
+class Cube implements Shapes {
+    @Override
+    public void area(){}
+
+    @Override
+    public void volume(){}
+}
+public class before {
+    public static void main(String[] args){
+        Shapes s1 = new Square();
+        Shapes s2 = new Cube();
+
+        s1.area();
+        s1.volume(); // this will thrown an exception
+
+        s2.area();
+        s2.volume();
+    }
+}
