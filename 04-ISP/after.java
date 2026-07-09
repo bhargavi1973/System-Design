ace96c38c15fa822ed2ee6b7f3ddf6f459d84f9e
Bhargavi Kanojia <165196422+bhargavi1973@users.noreply.github.com>
2026-07-09 19:26:04 +0530

    Implement interface segregation for shapes
    Implemented interface segregation principle by separating 2D and 3D shape interfaces. The Shape2D interface only includes area() while Shape3D extends it to include volume().

---
+++ 04-ISP/after.java
@@ -0,0 +1,37 @@
+// Here, the classes of 2D shapes cannot access the volume method
+// but the classes of 3D shapes can access botj area() and volume() methods
+
+interface Shape2D{
+    void area();
+}
+
+interface Shape3D extends Shape2D{
+    void volume();
+}
+class square implements Shape2D{
+    @Override
+    public void area(){}
+}
+class rectangle implements Shape2D{
+    @Override 
+    public void area(){}
+}
+class cube implements Shape3D{
+    @Override
+    public void area(){}
+
+    @Override
+    public void volume(){}
+}
+public class after {
+    public static void main(String[] args){
+        Shape2D s1 = new square();
+        Shape3D s2 = new cube();
+
+        s1.area();
+        s1.volume();  // throws a cannot find symbol error because volume() method is not defined in Shape2D interface
+
+        s2.area();
+        s2.volume();
+    }
+}
