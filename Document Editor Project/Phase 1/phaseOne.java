38f6b002043c53fda8fe1eaaa6178e5c94bd3bff
Bhargavi Kanojia <165196422+bhargavi1973@users.noreply.github.com>
2026-07-14 23:03:56 +0530

    Create phaseOne.java

---
+++ Document Editor Project/Phase 1/phaseOne.java
@@ -0,0 +1,46 @@
+// PHASE 1 : Monolithic DocumentEditor
+
+class Document{
+    void createDocument(){
+
+    }
+    void addText(){
+
+    }
+    void deleteText(){
+        
+    }
+    void save(){
+
+    }
+    void load(){
+
+    }
+    void print(){
+
+    }
+    void exportPDF(){
+
+    }
+    void exportHTML(){
+
+    }
+    void spellCheck(){
+        
+    }
+    
+}
+public class phaseOne {
+    public static void main(String[] args){
+        Document doc = new Document();
+        doc.createDocument();
+        doc.addText();
+        doc.deleteText();
+        doc.save();
+        doc.load();
+        doc.print();
+        doc.exportPDF();
+        doc.exportHTML();
+        doc.spellCheck();
+    }
+}
